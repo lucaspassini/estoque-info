b0c92a1f8b80d4d9e6d9860f64c430ca25d88116
Lucas <passini@protonmail.com>
2020-10-25 17:30:16 -0300

    added mysql connection

--- EstoqueInfo/src/model/Main.java
+++ EstoqueInfo/src/model/Main.java
@@ -2,11 +2,8 @@ package model;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Usuario usuario1 = new Usuario();
-		Cliente cliente1 = new Cliente();
-		Fornecedor fornecedor1 = new Fornecedor();
-		Produto produto1 = new Produto();
-		Estoque estoque1 = new Estoque();
+	public static void main(String[] args) throws Exception {
+		 MySQLAccess dao = new MySQLAccess();
+		 dao.readDataBase();
    }
 }
